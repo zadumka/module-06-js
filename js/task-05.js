48329e7ac1058462b06e321f3baf1365b98d2664
zadumka <90329284+zadumka@users.noreply.github.com>
2023-07-05 14:17:33 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
-const refs = {
-    inputName: document.querySelector('#name-input'),
-    outputName: document.querySelector('#name-output'),
-};
+//const refs = {
+//    inputName: document.querySelector('#name-input'),
+//    outputName: document.querySelector('#name-output'),
+//};
 
-refs.inputName.addEventListener('input', event => {
-    refs.outputName.textContent = event.currentTarget.value.trim() || 'Anonymous';
-});
+//refs.inputName.addEventListener('input', event => {
+//    refs.outputName.textContent = event.currentTarget.value.trim() || 'Anonymous';
+//});
